bebae2b6a84c7804412a31650bbf523dc9353a8f
Valtteri <n6niva00@students.oamk.fi>
2020-01-31 22:28:13 +0200

    bodytype to static when holding pieces

--- Assets/Scripts/DragNDrop.cs
+++ Assets/Scripts/DragNDrop.cs
@@ -17,8 +17,11 @@ public class DragNDrop : MonoBehaviour
     void Update()
     {
 
+        // casting ray down 
         hit = Physics2D.Raycast(this.transform.position + new Vector3(0, -0.24f, 0), -transform.up, 0.1f, 1 << 8);
         Debug.DrawRay(this.transform.position + new Vector3(0, -0.24f, 0), -transform.up);
+
+        //if ray hits something we change boolean "isOnTopOfSomething" to true and false if it doesn't hit anything
         if (hit.collider != null)
         {
             isOnTopOfSomething = true;
@@ -27,27 +30,39 @@ public class DragNDrop : MonoBehaviour
         {
             isOnTopOfSomething = false;
         }
+        
+
         Debug.Log(isOnTopOfSomething);
+
+
+        //when piece is held by mouse/touch
         if (isBeingHeld == true)
         {
-            this.gameObject.GetComponent<Rigidbody2D>().gravityScale = 0;
+            //gravity is disabled and piece follows mouses/touches position
+            this.gameObject.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
             Vector3 mousePos;
             mousePos = Input.mousePosition;
             mousePos = Camera.main.ScreenToWorldPoint(mousePos);
-            //transform.rotation = Quaternion.identity;
+            transform.rotation = Quaternion.identity;
 
             this.gameObject.transform.localPosition = new Vector3(mousePos.x, mousePos.y, 0);
         }
 
         else
         {
-            this.gameObject.GetComponent<Rigidbody2D>().gravityScale = 1;
+            this.gameObject.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
         }
+
+        //if piece is on top of something and it has snapped to grid
+        //rigidbody changes from dynamic to kinematic
+
         if(isOnTopOfSomething == true)
         {
             if (isSnapped == true)
             {
                 this.gameObject.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Kinematic;
+                transform.rotation = Quaternion.identity;
+
             }
             else
             {
@@ -59,11 +74,9 @@ public class DragNDrop : MonoBehaviour
 
     private void OnMouseDown()
     {
+        //when player holds mouse or touch isBeingHeld changes to true
         if (Input.GetMouseButtonDown(0))
         {
-            Vector3 mousePos;
-            mousePos = Input.mousePosition;
-            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
             isBeingHeld = true;
         }
         
@@ -71,33 +84,40 @@ public class DragNDrop : MonoBehaviour
 
     private void OnMouseUp()
     {
+        //on realease isBeingHeld changes to false
         isBeingHeld = false;
-
+        
     }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if(collision.gameObject.tag == "GridSlot")
+        //when inside gridslots trigger piece snaps to it
+        if(isBeingHeld == false)
         {
-            if(isOnTopOfSomething == true)
+            if (collision.gameObject.tag == "GridSlot")
             {
-                this.transform.position = collision.gameObject.transform.position;
-                isSnapped = true;
-                gameObject.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Kinematic;
+                if (isOnTopOfSomething == true)
+                {
+                    this.transform.position = collision.gameObject.transform.position;
+                    isSnapped = true;
+                    gameObject.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Kinematic;
+                }
+                else
+                {
+                    isSnapped = false;
+                    gameObject.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+
+
+                }
+
             }
-            else
-            {
-                isSnapped = false;
-                gameObject.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
-
-
-            }
-
         }
+        
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
+        //when exiting trigger snapping stops
         if (collision.gameObject.tag == "GridSlot")
         {
             isSnapped = false;
