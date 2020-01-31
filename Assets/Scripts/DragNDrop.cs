32c11fb60bf5793d587bcdc747954bc7df2039fd
Valtteri <n6niva00@students.oamk.fi>
2020-01-31 21:03:44 +0200

    snapping

--- Assets/Scripts/DragNDrop.cs
+++ Assets/Scripts/DragNDrop.cs
@@ -9,7 +9,7 @@ public class DragNDrop : MonoBehaviour
     private float startPosX;
     private float startPosY;
     private bool isBeingHeld = false;
-    
+    private bool isSnapped = false;
     // Update is called once per frame
     void Update()
     {
@@ -18,8 +18,18 @@ public class DragNDrop : MonoBehaviour
             Vector3 mousePos;
             mousePos = Input.mousePosition;
             mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+            transform.rotation = Quaternion.identity;
 
             this.gameObject.transform.localPosition = new Vector3(mousePos.x, mousePos.y, 0);
+        }
+
+        if(isSnapped == true)
+        {
+            this.gameObject.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+        }
+        else
+        {
+            this.gameObject.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
         }
     }
 
@@ -39,5 +49,22 @@ public class DragNDrop : MonoBehaviour
     private void OnMouseUp()
     {
         isBeingHeld = false;
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "GridSlot")
+        {
+            this.transform.position = collision.gameObject.transform.position;
+            isSnapped = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "GridSlot")
+        {
+            isSnapped = false;
+        }
     }
 }
