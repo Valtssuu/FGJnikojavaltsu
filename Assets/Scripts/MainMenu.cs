50ed230d5c0afb787da83b7af4e6d14d2de94fe3
Valtteri <n6niva00@students.oamk.fi>
2020-02-01 13:54:16 +0200

    MainMenu and PauseCanvas

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public GameObject pausePanel;
+
+    public void TestLevel()
+    {
+        SceneManager.LoadScene("TestLevel");
+    }
+
+    public void PauseButton()
+    {
+        Time.timeScale = 0;
+        pausePanel.SetActive(true);
+    }
+    public void UnPause()
+    {
+        Time.timeScale = 1;
+        pausePanel.SetActive(false);
+    }
+    public void MainMenuButton()
+    {
+        SceneManager.LoadScene("MainMenu");
+        Time.timeScale = 1;
+    }
+}
