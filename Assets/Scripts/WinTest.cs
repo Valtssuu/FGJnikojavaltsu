08241b47ea32f13ce6cbe8dda5c6544b1556f598
ni5u <snissine@gmail.com>
2020-02-01 16:06:31 +0200

    Wintest

---
+++ Assets/Scripts/WinTest.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinTest : MonoBehaviour
+{
+    List<GameObject> Pieces = new List<GameObject>();
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach(GameObject Piece in GameObject.FindGameObjectsWithTag("Piece"))
+        {
+            Pieces.Add(Piece);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (CheckWin())
+        {
+            Debug.Log("Win!");
+        }
+    }
+
+    private bool CheckWin()
+    {
+        foreach (GameObject Piece in Pieces)
+        {
+            RedCube redCube = Piece.GetComponent<RedCube>();
+            if (redCube == null)
+            {
+                if (Piece.GetComponent<YellowCube>().isSnapped == false)
+                    return false;
+            }
+            else
+            {
+                if (Piece.GetComponent<RedCube>().isSnapped == false)
+                {
+                    return false;
+                }
+            }
+               
+        }
+        return true;
+    }
+}
